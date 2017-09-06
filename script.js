6f1f66130b60fc079976b727ac6a53e04c121c2e
dhuber666 <dhuber666@gmail.com>
2017-09-06 19:43:37 +0200

    Finished Project

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var colors = generateRandomColors(6);
+
+var numSquares = 6;
+
+var colors = generateRandomColors(numSquares);
+
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -10,10 +14,12 @@ var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn")
 var hardBtn = document.querySelector("#hardBtn")
 
+
 easyBtn.addEventListener("click", function() {
     easyBtn.classList.add("selected");
     hardBtn.classList.remove("selected");
-    colors = generateRandomColors(3);
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for (var i = 0; i < squares.length; i++) {
@@ -29,8 +35,8 @@ easyBtn.addEventListener("click", function() {
 hardBtn.addEventListener("click", function() {
     easyBtn.classList.remove("selected");
     hardBtn.classList.add("selected");
-
-    colors = generateRandomColors(6);
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for (var i = 0; i < squares.length; i++) {
@@ -44,7 +50,7 @@ hardBtn.addEventListener("click", function() {
 
 resetButton.addEventListener("click", function() {
     // generate all new colors 
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     // pick a new random color from array
     pickedColor = pickColor();
     // change colorDisplay to match picked color
@@ -55,7 +61,8 @@ resetButton.addEventListener("click", function() {
         
     }
 
-    h1.style.background = "#232323";
+    h1.style.background = "steelblue";
+    message.textContent = "";
 })
 
 
